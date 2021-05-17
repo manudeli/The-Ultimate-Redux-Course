f9d69309f68a2fd100b11210e59da75ee970755e
flowithus <flowithusdev@gmail.com>
2021-05-17 21:33:51 +0900

    Redux Fundamentals - Create Reducer, Store, Dispatch Actions, Un/Subscribe

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import store from "./store";
+
+const unsubscribe = store.subscribe(() => {
+  console.log("Store changed!", store.getState());
+});
+
+store.dispatch({
+  type: "bugAdded",
+  payload: { description: "Bug1" },
+});
+
+unsubscribe();
+
+store.dispatch({
+  type: "bugRemoved",
+  payload: { id: 1 },
+});
+
+console.log(store.getState());
