37bdcd3e912250aaba6b4817f96003663831d37c
flowithus <flowithusdev@gmail.com>
2021-05-17 22:25:01 +0900

    05. Subscribing to the Store

--- src/customStore.js
+++ src/customStore.js
@@ -2,16 +2,23 @@ import reducer from "./reducer";
 
 function createStore(reducer) {
   let state;
+  let listeners = [];
+
+  function subscribe(listener) {
+    listeners.push(listener);
+  }
 
   function dispatch(action) {
     state = reducer(state, action);
+
+    for (let i = 0; i < listeners.length; i++) listeners[i]();
   }
 
   function getState() {
     return state;
   }
 
-  return { dispatch, getState };
+  return { dispatch, getState, subscribe };
 }
 
 export default createStore(reducer);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import store from "./customStore";
 import * as actions from "./actions";
 
+store.subscribe(() => {
+  console.log("Store changed!");
+});
+
 store.dispatch(actions.bugAdded("Bug 1"));
 
 console.log(store.getState());
