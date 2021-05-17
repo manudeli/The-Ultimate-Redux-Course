0b2ab151afe298469ce91b654ac71f0b868d7d73
flowithus <flowithusdev@gmail.com>
2021-05-17 17:51:04 +0900

    Finish Functional Programming in JavaScript

---
+++ src/functional/composition.js
@@ -0,0 +1,10 @@
+import { compose, pipe } from "lodash/fp";
+
+let input = "   JavaScript    ";
+
+const trim = (str) => str.trim();
+const wrap = (type) => (str) => `<${type}>${str}</${type}>`;
+const toLowerCase = (str) => str.toLowerCase();
+
+const transform = pipe(trim, toLowerCase, wrap("div"));
+console.log(transform(input));
