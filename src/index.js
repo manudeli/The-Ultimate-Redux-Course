1cc95645da612d26ccbaa9a3224c1cce5304a48d
flowithus <flowithusdev@gmail.com>
2021-05-17 17:22:56 +0900

    13. Immutable.js

--- src/index.js
+++ src/index.js
@@ -8,3 +8,17 @@ const toLowerCase = (str) => str.toLowerCase();
 
 const transform = pipe(trim, toLowerCase, wrap("div"));
 console.log(transform(input));
+
+// Immutable.js
+
+import { Map } from "immutable";
+
+let book = Map({ title: "Harry Potter" });
+
+function publish(book) {
+  return book.set("isPublished", true);
+}
+
+book = publish(book);
+
+console.log(book.toJS());
