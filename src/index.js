8a259463fe2e520ab6f7a8068f22c7f8bfd6c882
flowithus <flowithusdev@gmail.com>
2021-05-18 10:46:16 +0900

    Creating projects slice

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import configureStore from "./store/configureStore";
 import * as actions from "./store/bugs";
+import { projectAdded } from "./store/projects";
 
 const store = configureStore();
 
@@ -7,9 +8,10 @@ store.subscribe(() => {
   console.log("Store changed!");
 });
 
-store.dispatch(actions.bugAdded({ description: "Bug 1" }));
-store.dispatch(actions.bugAdded({ description: "Bug 2" }));
-store.dispatch(actions.bugAdded({ description: "Bug 3" }));
+store.dispatch(projectAdded({ name: "Project 1" }));
+store.dispatch(actions.bugAdded({ description: "Bug1" }));
+store.dispatch(actions.bugAdded({ description: "Bug2" }));
+store.dispatch(actions.bugAdded({ description: "Bug3" }));
 store.dispatch(actions.bugResolved({ id: 1 }));
 
 console.log(store.getState());
