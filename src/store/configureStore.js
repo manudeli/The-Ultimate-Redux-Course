ccf5bc550c5d2adaa501d95061709410aa1d28c0
flowithus <flowithusdev@gmail.com>
2021-05-18 17:25:08 +0900

    Use getDefaultMiddleware containing into Reduxtoolkit

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,10 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import reducer from "./reducer";
 import logger from "./middleware/logger";
-import func from "./middleware/func";
 
 export default function () {
   return configureStore({
     reducer,
-    middleware: [logger({ destination: "console" }), func],
+    middleware: [...getDefaultMiddleware(), logger({ destination: "console" })],
   });
 }
