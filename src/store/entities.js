6a6ce212e95de39e2685a20a7f03df2af25a5225
flowithus <flowithusdev@gmail.com>
2021-05-18 13:29:45 +0900

    Combining Reducers

---
+++ src/store/entities.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+import bugsReducer from "./bugs";
+import projectsReducer from "./projects";
+
+export default combineReducers({
+  bugs: bugsReducer,
+  projects: projectsReducer,
+});
