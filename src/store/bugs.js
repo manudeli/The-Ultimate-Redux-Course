ac14f7d3ade08daf522f81f111d735fdeb0607e8
flowithus <flowithusdev@gmail.com>
2021-05-18 09:41:44 +0900

    Creating Actions

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,28 +1,15 @@
-// Action types
-const BUG_ADDED = "bugAdded";
-const BUG_REMOVED = "bugRemoved";
-const BUG_RESOLVED = "bugResolved";
+import { createAction } from "@reduxjs/toolkit";
 
 // Action creators
-export const bugAdded = (description) => ({
-  type: BUG_ADDED,
-  payload: {
-    description,
-  },
-});
-
-export const bugResolved = (id) => ({
-  type: BUG_RESOLVED,
-  payload: {
-    id,
-  },
-});
+export const bugAdded = createAction("bugAdded");
+export const bugResolved = createAction("bugResolved");
+export const bugRemoved = createAction("bugRemoved");
 
 // Reducer
 let lastId = 0;
 export default function reducer(state = [], action) {
   switch (action.type) {
-    case BUG_ADDED:
+    case bugAdded.type:
       return [
         ...state,
         {
@@ -31,12 +18,14 @@ export default function reducer(state = [], action) {
           resolved: false,
         },
       ];
-    case BUG_RESOLVED:
+
+    case bugRemoved.type:
+      return state.filter((bug) => bug.id !== action.payload.id);
+
+    case bugResolved.type:
       return state.map((bug) =>
         bug.id !== action.payload.id ? bug : { ...bug, resolved: true }
       );
-    case BUG_REMOVED:
-      return state.filter((bug) => bug.id !== action.payload.id);
 
     default:
       return state;
