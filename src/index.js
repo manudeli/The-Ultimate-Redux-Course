356d93eef7d47af83497da1989bfa2818ad7ae7c
flowithus <flowithusdev@gmail.com>
2021-05-18 09:25:18 +0900

    Refactor as Ducks Pattern

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import store from "./store";
-import * as actions from "./actions";
+import configureStore from "./store/configureStore";
+import * as actions from "./store/bugs";
+
+const store = configureStore();
 
 store.subscribe(() => {
   console.log("Store changed!");
--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,11 +1,28 @@
-import * as actions from "./actionType";
+// Action types
+const BUG_ADDED = "bugAdded";
+const BUG_REMOVED = "bugRemoved";
+const BUG_RESOLVED = "bugResolved";
 
-// []
+// Action creators
+export const bugAdded = (description) => ({
+  type: BUG_ADDED,
+  payload: {
+    description,
+  },
+});
+
+export const bugResolved = (id) => ({
+  type: BUG_RESOLVED,
+  payload: {
+    id,
+  },
+});
+
+// Reducer
 let lastId = 0;
-
 export default function reducer(state = [], action) {
   switch (action.type) {
-    case actions.BUG_ADDED:
+    case BUG_ADDED:
       return [
         ...state,
         {
@@ -14,11 +31,11 @@ export default function reducer(state = [], action) {
           resolved: false,
         },
       ];
-    case actions.BUG_RESOLVED:
+    case BUG_RESOLVED:
       return state.map((bug) =>
         bug.id !== action.payload.id ? bug : { ...bug, resolved: true }
       );
-    case actions.BUG_REMOVED:
+    case BUG_REMOVED:
       return state.filter((bug) => bug.id !== action.payload.id);
 
     default:
