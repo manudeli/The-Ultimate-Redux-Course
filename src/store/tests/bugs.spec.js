0d480fb2e1e235ef9dfbb6e15d86ad8408ac03ab
flowithus <flowithusdev@gmail.com>
2021-05-19 16:31:55 +0900

    Writing Clean Test 1.Arrage 2.Act 3.Assert

--- src/store/tests/bugs.spec.js
+++ src/store/tests/bugs.spec.js
@@ -4,15 +4,37 @@ import { addBug } from "../bugs";
 import configureStore from "../configureStore";
 
 describe("bugsSlice", () => {
-  it("should handle the addBug action", async () => {
+  let fakeAxios;
+  let store;
+
+  beforeEach(() => {
+    fakeAxios = new MockAdapter(axios);
+    store = configureStore();
+  });
+
+  const bugsSlice = () => store.getState().entities.bugs;
+
+  it("should add the bug to the store if it's saved to the server", async () => {
+    // Three part of Tip for writing Clean code to unit test
+
+    // 1. Arrange:: All the initialization code
     const bug = { description: "a" };
     const savedBug = { ...bug, id: 1 };
-
-    const fakeAxios = new MockAdapter(axios);
     fakeAxios.onPost("/bugs").reply(200, savedBug);
 
-    const store = configureStore();
+    // 2. Act:: The code for poking the system or triggering action
     await store.dispatch(addBug(bug));
-    expect(store.getState().entities.bugs.list).toContainEqual(savedBug);
+
+    // 3. Assert:: The expectation code
+    expect(bugsSlice().list).toContainEqual(savedBug);
+  });
+
+  it("should not add the bug to the store if it's not saved to the server", async () => {
+    const bug = { description: "a" };
+    fakeAxios.onPost("/bugs").reply(500);
+
+    await store.dispatch(addBug(bug));
+
+    expect(bugsSlice().list).toHaveLength(0);
   });
 });
