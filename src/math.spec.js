ad6f5f9a6eba7b9190d7cb6d335a9914e8f9dd7c
flowithus <flowithusdev@gmail.com>
2021-05-19 11:10:15 +0900

    Add My first Jest testing code

--- src/math.spec.js
+++ src/math.spec.js
@@ -1,1 +1,14 @@
-it("first test", () => {});
+import { isEven } from "./math";
+
+describe("isEven", () => {
+  it("should return true if given an even number", () => {
+    // Function under test (SUT::: System Under Test)
+    const result = isEven(2);
+    expect(result).toEqual(true);
+  });
+
+  it("should return false if given an odd number", () => {
+    const result = isEven(1);
+    expect(result).toEqual(false);
+  });
+});
